361d1e7d28556259a974ff6fc52f48ca3e20e31c
Sukhi <sm_719@hotmail.com>
2013-11-03 11:15:38 -0800

    Initial project commit for the MOA app

---
+++ MOA/MOA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MOA
+//
+//  Created by Sukhi Mann on 11/3/2013.
+//  Copyright (c) 2013 Museum of Anthropology UBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

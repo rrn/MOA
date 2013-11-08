0757de2f7cba64dd0585567b592f2a4c124e87ac
Diana Sutandie <dianasutandie@gmail.com>
2013-11-07 17:36:25 -0800

    Added navigation functionality in side bar for Visitor Information and What’s on This Week

--- MOA/MOA/SidebarViewController.m
+++ MOA/MOA/SidebarViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "SidebarViewController.h"
+#import "SWRevealViewController.h"
+//#import "WhatsOnThisWeekViewController.h"
+
 
 
 @interface SidebarViewController ()
@@ -28,8 +31,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
+    self.tableView.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
+    self.tableView.separatorColor = [UIColor colorWithWhite:0.15f alpha:0.2f];
+    
+    _menuItems = @[@"Cell0", @"Cell1", @"Cell2", @"Cell3", @"Cell4", @"Cell5", @"Cell6", @"Cell7"];
     
     
+}
+
+- (void) prepareForSegue: (UIStoryboardSegue *) segue sender: (id) sender
+{
+
+    
+        // Set the title of navigation bar by using the menu items
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        UINavigationController *destViewController = (UINavigationController*)segue.destinationViewController;
+        destViewController.title = [[_menuItems objectAtIndex:indexPath.row] capitalizedString];
+    
+        if ( [segue isKindOfClass: [SWRevealViewControllerSegue class]] ) {
+            SWRevealViewControllerSegue *swSegue = (SWRevealViewControllerSegue*) segue;
+        
+            swSegue.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc) {
+            
+            UINavigationController* navController = (UINavigationController*)self.revealViewController.frontViewController;
+            [navController setViewControllers: @[dvc] animated: NO ];
+            [self.revealViewController setFrontViewPosition: FrontViewPositionLeft animated: YES];
+        };
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,16 +79,13 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 0;
+    return [self.menuItems count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"cell";
+    NSString *CellIdentifier = [self.menuItems objectAtIndex:indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
     
     return cell;
 }
---
+++ MOA/MOA/WhatsOnThisWeekViewController.m
@@ -0,0 +1,47 @@
+//
+//  WhatsOnThisWeekViewController.m
+//  MOA
+//
+//  Created by Diana Sutandie on 11/7/2013.
+//  Copyright (c) 2013 Museum of Anthropology UBC. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
+#import "WhatsOnThisWeekViewController.h"
+
+@interface WhatsOnThisWeekViewController ()
+
+@end
+
+@implementation WhatsOnThisWeekViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"This Week at MOA";
+    
+    // Set the side bar button action. When it's tapped, it'll show up the sidebar.
+    _sidebarButton.target = self.revealViewController;
+    _sidebarButton.action = @selector(rightRevealToggle:);
+    
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

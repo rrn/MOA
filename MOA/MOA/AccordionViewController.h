bfee55a4b51ed390e2aad852ec9ecbd7054ec943
marilyne <marilynedgar@Jordons-MacBook-Air.local>
2013-11-20 21:47:47 -0800

    Added a table view to test the accordion feature.

---
+++ MOA/MOA/AccordionViewController.h
@@ -0,0 +1,16 @@
+//
+//  AccordionViewController.h
+//  MOA
+//
+//  Created by Marilyn Edgar on 11/19/2013.
+//  Copyright (c) 2013 Museum of Anthropology UBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccordionViewController : UITableViewController
+{
+    bool sectionopen[4]; 
+}
+
+@end
---
+++ MOA/MOA/AccordionViewController.m
@@ -0,0 +1,152 @@
+//
+//  AccordionViewController.m
+//  MOA
+//
+//  Created by Marilyn Edgar on 11/19/2013.
+//  Copyright (c) 2013 Museum of Anthropology UBC. All rights reserved.
+//
+
+#import "AccordionViewController.h"
+
+@interface AccordionViewController ()
+
+@end
+
+@implementation AccordionViewController
+{
+    NSArray *tableData;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+    tableData = [NSArray arrayWithObjects:@"Location",@"Directions & Parking", @"Hours", @"Rates", nil];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath  {
+	if (sectionopen[indexPath.row]) {
+		return 240;///it's open
+	} else {
+		return 45;///it's closed
+	}
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text= [tableData objectAtIndex:indexPath.row];
+    return cell;
+    
+    
+    UITableViewCell *mycell;
+	mycell.textLabel.text= @"Section Name";
+	return mycell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	///turn them all off
+	sectionopen[0]=NO;
+	sectionopen[1]=NO;
+	sectionopen[2]=NO;
+	sectionopen[3]=NO;
+    
+	///open this one
+	sectionopen[indexPath.row]=YES;
+    
+	///animate the opening and expand the row
+	[self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
+}
+
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end

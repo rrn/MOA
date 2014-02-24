2420f09d8803c87744c6355c5c8da238909f34e6
marilyne <marilyn74@gmail.com>
2014-02-23 23:45:59 -0800

    Change youtube to a web view. Add social media icons to the sidebar menu.

--- MOA/YoutubeViewController.h
+++ MOA/YoutubeViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface YoutubeViewController : UIViewController
+{
+    IBOutlet UIWebView *youtubeWebView;
+}
 
 @end
--- MOA/YoutubeViewController.m
+++ MOA/YoutubeViewController.m
@@ -28,19 +28,24 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    NSString *channelName = @"MUSEUMofANTHROPOLOGY";
+    NSString *htmlString = @"<html><head><meta name = \"viewport\" content = \"initial-scale = 1.0, user-scalable = no, width = 212\"/></head><body style=\"background:#F00;margin-top:0px;margin-left:0px\"><div><object width=\"212\" height=\"172\"><param name=\"movie\" value=\"http://www.youtube.com/v/oHg5SJYRHA0&f=gdata_videos&c=ytapi-my-clientID&d=nGF83uyVrg8eD4rfEkk22mDOl3qUImVMV6ramM\"></param><param name=\"wmode\" value=\"transparent\"></param><embed src=\"http://www.youtube.com/v/oHg5SJYRHA0&f=gdata_videos&c=ytapi-my-clientID&d=nGF83uyVrg8eD4rfEkk22mDOl3qUImVMV6ramM\"type=\"application/x-shockwave-flash\" wmode=\"transparent\" width=\"212\" height=\"172\"></embed></object></div></body></html>";
     
-    NSURL *linkToAppURL = [NSURL URLWithString:[NSString stringWithFormat:@"youtube://user/%@",channelName]];
-    NSURL *linkToWebURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com/user/%@",channelName]];
-    
-    if ([[UIApplication sharedApplication] canOpenURL:linkToAppURL]) {
-        // Can open the youtube app URL so launch the youTube app with this URL
-        [[UIApplication sharedApplication] openURL:linkToAppURL];
-    }
-    else{
-        // Can't open the youtube app URL so launch Safari instead
-        [[UIApplication sharedApplication] openURL:linkToWebURL];
-    }
+    [youtubeWebView loadHTMLString:htmlString baseURL:[NSURL URLWithString:@"http://www.your-url.com"]];
+//    
+//    
+//    NSString *channelName = @"MUSEUMofANTHROPOLOGY";
+//    
+//    NSURL *linkToAppURL = [NSURL URLWithString:[NSString stringWithFormat:@"youtube://user/%@",channelName]];
+//    NSURL *linkToWebURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com/user/%@",channelName]];
+//    
+//    if ([[UIApplication sharedApplication] canOpenURL:linkToAppURL]) {
+//        // Can open the youtube app URL so launch the youTube app with this URL
+//        [[UIApplication sharedApplication] openURL:linkToAppURL];
+//    }
+//    else{
+//        // Can't open the youtube app URL so launch Safari instead
+//        [[UIApplication sharedApplication] openURL:linkToWebURL];
+//    }
 }
 
 - (void)didReceiveMemoryWarning
